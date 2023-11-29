8c8ab0212cae3b41d0a60ee718355eb3d4cfae13
Michael John Revilla <revilla.261443@gensan.sti.edu.ph>
2023-11-29 21:54:50 +0800

    Add project files.

---
+++ Basic Calculator/FrmBasicCalculator.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Basic_Calculator
+{
+    partial class FrmBasicCalculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNum1 = new System.Windows.Forms.TextBox();
+            this.cbOp = new System.Windows.Forms.ComboBox();
+            this.txtNum2 = new System.Windows.Forms.TextBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.btnCompute = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 15F);
+            this.label1.Location = new System.Drawing.Point(7, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(173, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Basic Calculator";
+            // 
+            // txtNum1
+            // 
+            this.txtNum1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtNum1.Font = new System.Drawing.Font("Verdana", 14F);
+            this.txtNum1.Location = new System.Drawing.Point(12, 61);
+            this.txtNum1.Name = "txtNum1";
+            this.txtNum1.Size = new System.Drawing.Size(318, 30);
+            this.txtNum1.TabIndex = 1;
+            this.txtNum1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // cbOp
+            // 
+            this.cbOp.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.cbOp.Font = new System.Drawing.Font("Verdana", 14F);
+            this.cbOp.FormattingEnabled = true;
+            this.cbOp.Items.AddRange(new object[] {
+            "+",
+            "-",
+            "*",
+            "/"});
+            this.cbOp.Location = new System.Drawing.Point(130, 98);
+            this.cbOp.Name = "cbOp";
+            this.cbOp.Size = new System.Drawing.Size(78, 31);
+            this.cbOp.TabIndex = 2;
+            // 
+            // txtNum2
+            // 
+            this.txtNum2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtNum2.Font = new System.Drawing.Font("Verdana", 14F);
+            this.txtNum2.Location = new System.Drawing.Point(12, 135);
+            this.txtNum2.Name = "txtNum2";
+            this.txtNum2.Size = new System.Drawing.Size(318, 30);
+            this.txtNum2.TabIndex = 3;
+            this.txtNum2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtResult
+            // 
+            this.txtResult.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtResult.BackColor = System.Drawing.SystemColors.ControlText;
+            this.txtResult.Font = new System.Drawing.Font("Verdana", 14F);
+            this.txtResult.ForeColor = System.Drawing.Color.Lime;
+            this.txtResult.Location = new System.Drawing.Point(12, 171);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(318, 115);
+            this.txtResult.TabIndex = 4;
+            // 
+            // btnCompute
+            // 
+            this.btnCompute.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btnCompute.Font = new System.Drawing.Font("Verdana", 12F);
+            this.btnCompute.Location = new System.Drawing.Point(115, 303);
+            this.btnCompute.Name = "btnCompute";
+            this.btnCompute.Size = new System.Drawing.Size(111, 40);
+            this.btnCompute.TabIndex = 5;
+            this.btnCompute.Text = "Compute";
+            this.btnCompute.UseVisualStyleBackColor = true;
+            this.btnCompute.Click += new System.EventHandler(this.btnCompute_Click);
+            // 
+            // FrmBasicCalculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(342, 355);
+            this.Controls.Add(this.btnCompute);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.txtNum2);
+            this.Controls.Add(this.cbOp);
+            this.Controls.Add(this.txtNum1);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmBasicCalculator";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FrmBasicCalculator";
+            this.Load += new System.EventHandler(this.FrmBasicCalculator_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNum1;
+        private System.Windows.Forms.ComboBox cbOp;
+        private System.Windows.Forms.TextBox txtNum2;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Button btnCompute;
+    }
+}
+
---
+++ Basic Calculator/FrmBasicCalculator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CalculatorPrivateAssembly;
+namespace Basic_Calculator
+{
+    public partial class FrmBasicCalculator : Form
+    {
+        public FrmBasicCalculator()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmBasicCalculator_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCompute_Click(object sender, EventArgs e)
+        {
+            switch (cbOp.SelectedItem.ToString())
+            {
+                case "+":
+                    float total = BasicComputation.Addition(float.Parse(txtNum1.Text), float.Parse(txtNum2.Text));
+                    txtResult.Text = $"Total:\r\n{total}";
+                    break;
+                case "-":
+                    float total2 = BasicComputation.Subtraction(float.Parse(txtNum1.Text), float.Parse(txtNum2.Text));
+                    txtResult.Text = $"Total:\r\n{total2}";
+                    break;
+                case "*":
+                    float total3 = BasicComputation.Multiplication(float.Parse(txtNum1.Text), float.Parse(txtNum2.Text));
+                    txtResult.Text = $"Total:\r\n{total3}";
+                    break;
+                case "/":
+                    float total4 = BasicComputation.Division(float.Parse(txtNum1.Text), float.Parse(txtNum2.Text));
+                    txtResult.Text = $"Total:\r\n{total4}";
+                    break;
+            }
+        }
+    }
+}
